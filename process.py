49eb636cb99a595a53b486876dd0cd67562d3f4e
jasonlin1222 <jasonlin1222@gmail.com>
2019-03-04 19:46:03 +0800

    upload

--- process.py
+++ process.py
@@ -1,11 +1,10 @@
 import ev3dev.ev3 as ev3
 import time
 import pickle as pk
-import data as dt
 
-# startsys():
-#     pass
+startsys():
+    pass
 
-# endsys():
-#     with open("userdata.txt", "w+") as file:
-#         pk.dump()
+endsys():
+    with open("userdata.txt", "w+") as file:
+        pk.dump()
