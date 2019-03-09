7d69aa4f6f446bf12d8325159538d640af71d175
jasonlin1222 <jasonlin1222@gmail.com>
2019-03-09 22:53:13 +0800

    upload

--- system.py
+++ system.py
@@ -5,6 +5,7 @@ import ui as i
 colors = ('unknown black blue green yellow red white brown'.split())
 while True
 	if i.ts.is_pressed:
+		trash = True
 		print("trash in")
 		print("insert your card and hold still.....")
 		time.sleep(5)
@@ -12,4 +13,6 @@ while True
 		while card is unknown:
 			print("card is not detected")
 			card = colors[i.coldoor.value()]
-		i.doorm.run_to_rel_pos(speed_sp = , position_sp = ) #set block working
+		i.doorm.run_to_rel_pos(speed_sp = 100, position_sp = 90) #opmitize result 
+		while colors[i.colin.value()] is unknown:
+			print("waiting for trash...")
