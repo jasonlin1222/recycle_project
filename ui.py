7c72aabdda5c041b2761e879545257a270f78890
jasonlin1222 <jasonlin1222@gmail.com>
2019-03-16 14:54:26 +0800

    upload

--- ui.py
+++ ui.py
@@ -50,11 +50,8 @@ def start():
 				break
 		if coloru not in user:
 			user[coloru] = usercolor()
-			while True:
-				user[coloru].username = input('enter your name: ')
-				user[coloru].password = input('enter your password: ')
-				if re.match("[a-zA-Z0-9]{n, m}", user[coloru.username]):
-					break
+			user[coloru].username = input('enter your name: ')
+			user[coloru].password = input('enter your password: ')
 		else:
 			print("user already exist")
 		# exec(coloru + '= usercolor()')
