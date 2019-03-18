a1b1f9bdeb7adc75cfafe7fe71c29aa8c75981be
jasonlin1222 <jasonlin1222@gmail.com>
2019-03-18 19:45:45 +0800

    upload

--- ui.py
+++ ui.py
@@ -23,12 +23,12 @@ colors = ('unknown black blue green yellow red white brown'.split())
 user = dict()
 #init end
 class usercolor:
-def __init__(self):
-	self.password = ''
-	self.money = 0
-	self.trash = 0
-	self.recycle = 0
-	self.username = ''
+	def __init__(self):
+		self.password = ''
+		self.money = 0
+		self.trash = 0
+		self.recycle = 0
+		self.username = ''
 
 def start():
 	if os.path.isfile("./userdata.txt"):
