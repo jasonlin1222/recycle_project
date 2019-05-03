d227d0777bf0e543f2a5becea5f8f3e8c233bb27
jasonlin1222 <jasonlin1222@gmail.com>
2019-05-03 15:28:27 +0000

    last upload

--- car/main.py
+++ car/main.py
@@ -6,16 +6,23 @@ col = ev3.ColorSensor('in3')
 col.mode = 'COL-REFLECT'
 trigger = ev3.TouchSensor('in2')
 kp = 2
+ki = 0
+kd = 0
 offset = 31
 tp = -100
 track = True
+inter = 0
+lasterr = 0
 while True:
 	try:
 		while trigger.value():
 			error = col.value() - offset
-			turn = kp * error
+			inter = inter + error
+			der = error - lasterr
+			turn = kp * error + ki*inter
 			powerC = tp - turn
 			powerB = tp + turn
+			lasterr = error
 			rightm.run_forever(speed_sp = powerB)
 			leftm.run_forever(speed_sp = powerC)
 		rightm.stop()
