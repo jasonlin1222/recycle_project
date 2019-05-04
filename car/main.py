aa760beefac773f74a05d1bae7e3879e15228c7a
jasonlin1222 <jasonlin1222@gmail.com>
2019-05-04 05:02:48 +0000

    upload

--- car/main.py
+++ car/main.py
@@ -5,11 +5,11 @@ leftm = ev3.LargeMotor('outB')
 col = ev3.ColorSensor('in3')
 col.mode = 'COL-REFLECT'
 trigger = ev3.TouchSensor('in2')
-kp = 2
-ki = 0
-kd = 0
-offset = 31
-tp = -100
+kp = 1.5
+ki = 0.15
+kd = 15
+offset = 30
+tp = -150
 track = True
 inter = 0
 lasterr = 0
@@ -19,12 +19,13 @@ while True:
 			error = col.value() - offset
 			inter = inter + error
 			der = error - lasterr
-			turn = kp * error + ki*inter
+			turn = kp * error + ki*inter + kd*der
 			powerC = tp - turn
 			powerB = tp + turn
 			lasterr = error
 			rightm.run_forever(speed_sp = powerB)
 			leftm.run_forever(speed_sp = powerC)
+			print(turn)
 		rightm.stop()
 		leftm.stop()
 	except KeyboardInterrupt:
